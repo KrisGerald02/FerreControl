e9304a2d2defed2f70f7d7c48be4ef56073065e7
WJLVuam20 <wjalopez@uam.edu.ni>
2025-11-27 15:17:42 -0600

    Stock maximo y minimo agregado

--- FerreControl/src/main/java/com/tuempresa/FerreControl/modelo/Producto.java
+++ FerreControl/src/main/java/com/tuempresa/FerreControl/modelo/Producto.java
@@ -29,6 +29,14 @@ public class Producto {
     @Min(0)
     private int stock;
 
+
+    @Min(0)
+    private int stockMinimo;
+
+
+    @Min(0)
+    private int stockMaximo;
+
     @Column(length=20)
     private String unidadMedida;
 }
