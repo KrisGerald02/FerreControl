696f4e8b469ea4d67201c145b1228436bd50806f
IntiMG <intimontalvan@uamv.edu.ni>
2025-12-01 22:04:00 -0600

    Fix categoriaProducto

--- FerreControl/src/main/java/com/tuempresa/FerreControl/modelo/Producto.java
+++ FerreControl/src/main/java/com/tuempresa/FerreControl/modelo/Producto.java
@@ -7,6 +7,14 @@ import org.openxava.annotations.*;
 import lombok.*;
 
 @Entity @Getter @Setter
+@View(
+    members =
+        "nombre;" +
+        "categoria;" +
+        "precioVenta, iva;" +
+        "stock, stockMinimo, stockMaximo;" +
+        "unidadMedida, estadoStock;"
+)
 public class Producto {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Hidden
